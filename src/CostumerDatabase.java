086c8e8d10134fb433ff710133ea3fcd23d42cb5
GJulienB <110825721+GJulienB@users.noreply.github.com>
2022-08-23 09:12:31 +0200

    Update CostumerDatabase.java

--- src/CostumerDatabase.java
+++ src/CostumerDatabase.java
@@ -3,29 +3,38 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
-public class CostumerDatabase {
-        public static void getCustomerData() {
+public class CustomerDatabase {
+    public static ArrayList<ArrayList<String>> getCustomerData() {
         String path = "C:\\Users\\BedrichJ\\Desktop\\Kundenliste\\kunden.csv";
         String line = "";
         BufferedReader br;
 
-        {
-                try {
-                      br = new BufferedReader(new FileReader(path));
-                        while ((line = br.readLine()) != null) {
-                          String[] values = line.split(",");
-                            System.out.println(values[0]);
+        ArrayList<ArrayList<String>> customer = new ArrayList<>();
 
-                        }
-                } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                } catch (IOException e) {
-                        e.printStackTrace();
-
+            try {
+                br = new BufferedReader(new FileReader(path));
+                while ((line = br.readLine()) != null) {
+                    String[] values = line.split("\n");
+                    for (String index : values){
+                        String[] customerPartsString = index.split(";");
+                        ArrayList<String> customerPartsArrayList = new ArrayList<>();
+                       //for(String datapoint : customerPartsString){
+                         //customerPartsArrayList.add(datapoint);
+                        //}
+                        Collections.addAll(customerPartsArrayList, customerPartsString);
+                        customer.add(customerPartsArrayList);
+                    }
                 }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+            return customer;
         }
 
 
-        }
-}
+    }
